edcbdfafd9458bd0046581e704e2a0b14bb86277
Nelson <nelson070585@gmail.com>
2024-02-03 21:14:47 -0300

    desarrollo parcial de funciones para traducción y correcciones de estilos

--- src/components/TranslateBox/TranslateBox.jsx
+++ src/components/TranslateBox/TranslateBox.jsx
@@ -1,27 +1,77 @@
-import { dataTranslate } from '../../data/dataTranslate.js'
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 import '../TranslateBox/TranslateBox.css'
 
 
 export const TranslateBox = () => {
 
-     const [word, setWord] = useState('');
+    let [words, setWords] = useState('');
+    const [languaje, setLanguaje] = useState('')
+    const [traductor, setTraductor] = useState([])
 
 
     const wordValidation = () =>{
-        console.log('cantidad de caracteres', word.length);
-        return word.length > 500 ? console.log("La palabra es mayor a 500 caracteres") : null;
+        console.log('cantidad de caracteres', words.length);
+        return words.length > 500 ? console.log("La palabra es mayor a 500 caracteres") : null;
     }
 
+   const english = 'en|en';
+   const englishToItalian = 'en|it';
+   const englishToSpanish = 'en|es';
+
+    
+    const translateBtn = async() =>{
+    const urlBase = `https://api.mymemory.translated.net/get?q=${words}&langpair=${languaje}`
+        
+        return axios.get(urlBase)
+                .then((res) =>{ return setTraductor(res.data.responseData.translatedText)})
+                .catch((err) =>{console.log(err);})
+    }
+
+
     useEffect(() =>{
-        dataTranslate();
-    },[])
+
+        translateBtn
+    })
+
+
 
     return(
         <div className='container'>
             <div className="containerB">
                 <div className="boxTranslateBtnA">
                     <span>Detected Languaje</span>
+                    <button onClick={() =>{return setLanguaje(english)}}>English</button>
+                    <button onClick={() =>{return setLanguaje(englishToItalian)}} >French</button>
+                    <button onClick={() =>{return setLanguaje(englishToSpanish)}}>Spanish<img src="public/images/expand_down.svg" alt="desplegar" /></button>
+                </div>
+
+                <textarea 
+                    name="textTranslate" 
+                    id="textTranslate" 
+                    value={words} 
+                    onChange={(e) => {
+                        wordValidation(); 
+                        setWords(e.target.value)
+                    }}
+                >
+                </textarea>
+                <span>{`${words.length}/500`}</span>
+                <div className='boxTranslateBtnB'>
+                    <section>
+                        <button><img src='public/images/sound_max_fill.svg' alt="altavoz" /></button>
+                        <button><img src="public/images/copy.svg" alt="copiar" /></button>
+                    </section>
+                    <section>
+                        <button id='btnTranslate' onClick={() => translateBtn()} ><img src="public/images/sort_alfa.svg" alt="alfa" />Translate</button>
+                    </section>
+                </div>
+            </div>
+
+                        {/* parte del segundo cuadro */}
+
+            <div className="containerB">
+                <div className="boxTranslateBtnA">
                     <button>English</button>
                     <button>French</button>
                     <button>Spanish<img src="public/images/expand_down.svg" alt="desplegar" /></button>
@@ -29,35 +79,11 @@ export const TranslateBox = () => {
 
                 <textarea 
                     name="textTranslate" 
-                    id="textTranslate" 
-                    value={word} 
-                    onChange={(e) => {
-                        wordValidation(); 
-                        setWord(e.target.value)
-                    }}
+                    id="textTranslate"
+                     value={traductor}
+                    // onChange={() => handleTraduction}
                 >
                 </textarea>
-                <span>{`${word.length}/500`}</span>
-                <div className='boxTranslateBtnB'>
-                    <section>
-                        <button><img src='public/images/sound_max_fill.svg' alt="altavoz" /></button>
-                        <button><img src="public/images/copy.svg" alt="copiar" /></button>
-                    </section>
-                    <section>
-                        <button id='btnTranslate'><img src="public/images/sort_alfa.svg" alt="alfa" />Translate</button>
-                    </section>
-                </div>
-
-            </div>
-
-            <div className="containerB">
-                <div className="boxTranslateBtnA">
-                    <button>English</button>
-                    <button>French</button>
-                    <button>Spanish<img src="public/images/expand_down.svg" alt="desplegar" /></button>
-                </div>
-
-                <textarea name="textTranslate" id="textTranslate"></textarea>
 
                 <div className='boxTranslateBtnB'>
                     <section>
--- src/data/dataTranslate.js
+++ src/data/dataTranslate.js
@@ -1,17 +1,17 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-    // const q = 'Hello World!';
-    // const langpair = 'en|it';
-export const dataTranslate = () =>{
+//     // const q = 'Hello World!';
+//     // const langpair = 'en|it';
+// export const dataTranslate = () =>{
     
-    // const urlBase = 'https://api.mymemory.translated.net/get?'
-    const urlBase = 'https://api.mymemory.translated.net/get?q=Hello%20World!&langpair=en|it'
+//     // const urlBase = 'https://api.mymemory.translated.net/get?'
+//     const urlBase = 'https://api.mymemory.translated.net/get?'
+//     //'https://api.mymemory.translated.net/get?q=Hello%20World!&langpair=en|it'
+//     return(
+//         axios.get(urlBase)
+//         .then((res) => console.log('Entro por then', res.data))
+//         .catch((err) => console.log('Entro por catch', err))
+//     )
 
-    return(
-        axios.get(urlBase)
-        .then((res) => console.log('Entro por then', res.data))
-        .catch((err) => console.log('Entro por catch', err))
-    )
-
-}
+// }
 
